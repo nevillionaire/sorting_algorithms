6abbb76357306cf34d92c37f9c2670a961f589e3
nevillionaire <nevillepeter12@gmail.com>
2022-07-18 23:43:26 +0300

    debug

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,31 +1,47 @@
 #include "sort.h"
 
 /**
- * selection_sort - sorts an array following the selection sort algorithm
- * @array: array to sort
- * @size: size of the array;
+ * swap - swaps two integers in an array
+ * @a: first number
+ * @b: second number
+ */
+void swap(int *a, int *b)
+{
+	int tmp = *a;
+
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * selection_sort - implementing the selection sort
+ * @array: the array to be sorted
+ * @size:size of the array
  */
 void selection_sort(int *array, size_t size)
 {
-	unsigned int i, j, min;
+	/**
+	 * look for the smallest element
+	 * swap the smallest element with the current
+	 */
+	unsigned int i = 0, j = 0, tmp_loc = 0;
+	int tmp;
 
-	register int tmp;
-
-	if (size < 2)
-		return;
-
-	for (i = 0; i < size; i++)
+	while (i < size)
 	{
-		min = i;
-		for (j = i + 1; j < size; j++)
+		j = i;
+		tmp = array[j], tmp_loc = j;
+		while (j < size)
 		{
-			if (array[j] < array[min])
-				min = j;
+			if (array[j] < tmp)
+				tmp = array[j], tmp_loc = j;
+			j++;
 		}
-		tmp = array[i];
-		array[i] = array[min];
-		array[min] = tmp;
-		if (i != min)
+		if (array[i] != array[tmp_loc])
+		{
+			swap(array + i, array + tmp_loc);
 			print_array(array, size);
+		}
+		i++;
 	}
 }
